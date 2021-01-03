93c6b3076f36b8bd87e227a82e0266704af52fbd
eugenedolgin <75638491+eugenedolgin@users.noreply.github.com>
2021-01-04 00:16:07 +0400

    Add files via upload

---
+++ test_items.py
@@ -0,0 +1,8 @@
+﻿import time
+
+link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+
+def test_button_add_to_basket(browser):
+    browser.get(link)
+    time.sleep(3)
+    assert len(browser.find_elements_by_css_selector("button.btn-add-to-basket")), "Button not exist"
